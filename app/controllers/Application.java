cdae1b108cbf763a0f9f61127a4e8261609ff047
JuanAntonio234 <127333032+JuanAntonio234@users.noreply.github.com>
2024-06-18 23:37:11 +0200

    ahawdaaaa

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,21 +26,19 @@ public class Application extends Controller {
         render();
     }
 
-    public static void registrar(String nameUser,int edad, String password, String confirmPassword) {
-        Usuario conductorExiste = Usuario.find("byName", nameUser).first();
-        if (conductorExiste != null) {
+    public static void registerUser(String nameUser, int edad, String password) {
+        Logger.info("Intentando registrar usuario: " + nameUser);
+
+        Usuario usuarioExistente = Usuario.find("byName", nameUser).first();
+        if (usuarioExistente != null) {
             renderText("El nombre de usuario ya está en uso. Por favor, elige otro nombre de usuario.");
         } else {
-            if (password.equals(confirmPassword)) {
-                Usuario newConductor = new Usuario(nameUser,edad, password,0).save();
-                session.put("usuario", nameUser);
-                Application.index();
-            } else {
-                renderText("Nombre de usuario o contraseña incorrectos.");
-            }
+            Usuario nuevoUsuario = new Usuario(nameUser, edad, password, 0).save();
+            Logger.info("Usuario registrado correctamente: " + nameUser);
+            session.put("usuario", nameUser);
+            render("Application/index.html");
         }
     }
-
     public static void registerAndroid(String nameUser,int edad, String password){
         Logger.info("Datos recibidos del cliente Android:");
         Logger.info("Nombre de usuario: " + nameUser);
@@ -71,19 +69,21 @@ public class Application extends Controller {
     }
 
     public static void login(String nameUser, String password) {
+        Logger.info("Usurio: " + nameUser,password);
+
         Usuario usuario = Usuario.find("byNameAndPassword", nameUser, password).first();
         if (usuario != null) {
             session.put("usuario", nameUser);
-            Application.index();
+          //  Application.index();
         } else {
             renderText("Nombre de usuario o contraseña incorrectos.");
         }
     }
 
-    public static void Login(){
+/* public static void login(){
         render();
     }
-
+*/
     public static void logout(){
         session.remove("usuario");
         renderText("Sesión cerrada exitosamente");
